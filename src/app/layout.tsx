e94ee8d226ab02e290e9617627aa171b7a04deb3
Jhonatan Monsalve <monsalve221090@gmail.com>
2026-01-28 23:33:50 +0100

    feat: add Google Tag Manager and auto-deployment workflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { SITE_CONFIG } from "@/lib/config/siteConfig";
 import { FloatingWhatsApp } from "@/components/ui/buttons/FloatingWhatsApp";
@@ -33,6 +34,29 @@ export default function RootLayout({
     return (
         <html lang="es" className={`${playfair.variable} ${inter.variable}`}>
             <body className="font-sans antialiased bg-background text-foreground">
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-5HFRPSX4"
+                        height="0"
+                        width="0"
+                        style={{ display: "none", visibility: "hidden" }}
+                    />
+                </noscript>
+
+                {/* Google Tag Manager */}
+                <Script
+                    id="gtm-script"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-5HFRPSX4');`,
+                    }}
+                />
+
                 {children}
                 <FloatingWhatsApp />
             </body>
