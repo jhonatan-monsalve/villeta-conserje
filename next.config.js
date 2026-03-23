d7195b0c69e84d535bc87fcee975c33a24dbb31e
Jhonatan Monsalve <monsalve221090@gmail.com>
2026-03-23 13:10:41 +0100

    fix: restaurar output export y server-dir ./ para corregir el despliegue root

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
         loader: "custom",
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
